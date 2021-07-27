85ba3288ede847159c9ad6c2ab3663c27e0e7ece
GiorgiyProjects <GiorgiyProjects@users.noreply.github.com>
2021-07-27 08:00:10 +0000

    deploy: 4bb81c024b4909381539d3d2ec57874b9cc77151

--- tests.cpp
+++ tests.cpp
@@ -33,14 +33,18 @@ BOOST_AUTO_TEST_SUITE( TestSuite )
             {
                 cout_redirect guard(output.rdbuf());
                 int argc = 3;
-                argv[0] = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
-                argv[1] = "--inc_dirs=../tests/test_include_exclude/";
-                argv[2] = "--scan_lvl=1";
+                string argv0,argv1,argv2;
+                argv0 = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
+                argv1 = "--inc_dirs=tests/test_include_exclude/";
+                argv2 = "--scan_lvl=1";
+                argv[0] = const_cast<char*>(argv0.c_str());
+                argv[1] = const_cast<char*>(argv1.c_str());
+                argv[2] = const_cast<char*>(argv2.c_str());
                 MainManager(argc, argv);
             }
-            BOOST_CHECK(output.is_equal("../tests/test_include_exclude/1.txt\n"
-                                           "../tests/test_include_exclude/1/1.txt\n"
-                                           "../tests/test_include_exclude/2/1.txt\n"
+            BOOST_CHECK(output.is_equal("tests/test_include_exclude/1.txt\n"
+                                           "tests/test_include_exclude/1/1.txt\n"
+                                           "tests/test_include_exclude/2/1.txt\n"
                                            "\n"));
         }
     }
@@ -51,15 +55,22 @@ BOOST_AUTO_TEST_SUITE( TestSuite )
             {
                 cout_redirect guard(output.rdbuf());
                 int argc = 4;
-                argv[0] = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
-                argv[1] = "--inc_dirs=../tests/test_include_exclude/";
-                argv[2] = "--exc_dirs=../tests/test_include_exclude/2/";
-                argv[3] = "--scan_lvl=1";
+                string argv0,argv1,argv2,argv3;
+                argv0 = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
+                argv1 = "--inc_dirs=tests/test_include_exclude/";
+                argv2 = "--exc_dirs=tests/test_include_exclude/2/";
+                argv3 = "--scan_lvl=1";
+
+                argv[0] = const_cast<char*>(argv0.c_str());
+                argv[1] = const_cast<char*>(argv1.c_str());
+                argv[2] = const_cast<char*>(argv2.c_str());
+                argv[3] = const_cast<char*>(argv3.c_str());
+
                 MainManager(argc, argv);
             }
-            BOOST_CHECK(output.is_equal("../tests/test_include_exclude/1.txt\n"
-                                        "../tests/test_include_exclude/1/1.txt\n"
-                                        "\n"));
+            BOOST_CHECK(output.is_equal("tests/test_include_exclude/1.txt\n"
+                                           "tests/test_include_exclude/1/1.txt\n"
+                                           "\n"));
         }
     }
     // to check if scan lvl flag = 0 works
@@ -69,13 +80,19 @@ BOOST_AUTO_TEST_SUITE( TestSuite )
             {
                 cout_redirect guard(output.rdbuf());
                 int argc = 3;
-                argv[0] = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
-                argv[1] = "--inc_dirs=../tests/test_scan_lvl/";
-                argv[2] = "--scan_lvl=0";
+                string argv0, argv1, argv2;
+                argv0 = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
+                argv1 = "--inc_dirs=tests/test_scan_lvl/";
+                argv2 = "--scan_lvl=0";
+
+                argv[0] = const_cast<char*>(argv0.c_str());
+                argv[1] = const_cast<char*>(argv1.c_str());
+                argv[2] = const_cast<char*>(argv2.c_str());
+
                 MainManager(argc, argv);
             }
-            BOOST_CHECK(output.is_equal("../tests/test_scan_lvl/1_1.txt\n"
-                                           "../tests/test_scan_lvl/1_2.txt\n"
+            BOOST_CHECK(output.is_equal("tests/test_scan_lvl/1_1.txt\n"
+                                           "tests/test_scan_lvl/1_2.txt\n"
                                            "\n"));
         }
     }
@@ -86,14 +103,20 @@ BOOST_AUTO_TEST_SUITE( TestSuite )
             {
                 cout_redirect guard(output.rdbuf());
                 int argc = 3;
-                argv[0] = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
-                argv[1] = "--inc_dirs=../tests/test_mask/";
-                argv[2] = "--mask=^[0-9]+$";
+                string argv0, argv1, argv2;
+                argv0 = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
+                argv1 = "--inc_dirs=tests/test_mask/";
+                argv2 = "--mask=^[0-9]+$";
+
+                argv[0] = const_cast<char*>(argv0.c_str());
+                argv[1] = const_cast<char*>(argv1.c_str());
+                argv[2] = const_cast<char*>(argv2.c_str());
+
                 MainManager(argc, argv);
             }
-            BOOST_CHECK(output.is_equal("../tests/test_mask/1\n"
-                                        "../tests/test_mask/12\n"
-                                        "\n"));
+            BOOST_CHECK(output.is_equal("tests/test_mask/1\n"
+                                           "tests/test_mask/12\n"
+                                           "\n"));
         }
     }
     // big test
@@ -103,21 +126,27 @@ BOOST_AUTO_TEST_SUITE( TestSuite )
             {
                 cout_redirect guard(output.rdbuf());
                 int argc = 2;
-                argv[0] = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
-                argv[1] = "--inc_dirs=../tests/test_big/";
-                argv[2] = "--exc_dirs=../tests/test_big/2/";
+                string argv0, argv1, argv2;
+                argv0 = "/home/bbrave/Desktop/OTUS/homeworks/hw7/HW7/cmake-build-debug/test";
+                argv1 = "--inc_dirs=tests/test_big/";
+                argv2 = "--exc_dirs=tests/test_big/2/";
+
+                argv[0] = const_cast<char*>(argv0.c_str());
+                argv[1] = const_cast<char*>(argv1.c_str());
+                argv[2] = const_cast<char*>(argv2.c_str());
+
                 MainManager(argc, argv);
             }
             BOOST_CHECK(output.is_equal(
-                    "../tests/test_big/1.1\n"
-                    "../tests/test_big/1/1.1/1.2\n"
-                    "../tests/test_big/3/3.1/1.3\n"
+                    "tests/test_big/1/1.1/2.2\n"
+                    "tests/test_big/1/2.1\n"
+                    "tests/test_big/2/2.3\n"
+                    "tests/test_big/2/2.4\n"
+                    "tests/test_big/3/2.5\n"
                     "\n"
-                    "../tests/test_big/1/1.1/2.2\n"
-                    "../tests/test_big/1/2.1\n"
-                    "../tests/test_big/2/2.3\n"
-                    "../tests/test_big/2/2.4\n"
-                    "../tests/test_big/3/2.5\n"
+                    "tests/test_big/1.1\n"
+                    "tests/test_big/1/1.1/1.2\n"
+                    "tests/test_big/3/3.1/1.3\n"
                     "\n"
                     ));
         }
